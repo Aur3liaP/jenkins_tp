3e080ce9a8670d263a19c5a02705936a27bd2039
Pic Aurélia <pic.aurelia@gmail.com>
2025-07-10 16:22:08 +0200

    Initial commit

---
+++ src/test/java/fr/diginamic/jenkins_tp/JenkinsTpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.jenkins_tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
